f3db77fc2b2a2b816f39554645eaa3438c873b18
r-richard404 <r.lazarica@yahoo.com>
2024-10-05 00:44:48 +0100

    Updated app.component.ts

--- country-search-app/src/app/app.component.ts
+++ country-search-app/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core'; //onInit used for ....
+import { CountryService } from './services/country.service';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'country-search-app';
+export class AppComponent implements OnInit {
+
+ countries: string[] = [];
+ searchQuery: string = '';
+
+ constructor(private countryService: CountryService) {}
+
+ ngOnInit() {
+   this.loadCountries();
+ }
+
+ loadCountries() {
+  this.countryService.getCountries().subscribe((data) => {
+    this.countries = data;
+  });
+}
+  searchCountries() {
+    this.countryService.searchCountries(this.searchQuery).subscribe((data) => {
+      this.countries = data;
+    });
+  }
 }
